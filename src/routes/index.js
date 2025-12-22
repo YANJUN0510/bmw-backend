1e1e9ccd62bc7a7abb9d4ffce5b55d72eeab97d1
Nolan Cui <ruipucui@gmail.com>
2025-12-22 19:59:09 +1100

    remove analytic router

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,13 +5,11 @@ const productRoutes = require('./productRoutes');
 const journalRoutes = require('./journalRoutes');
 const chatRoutes = require('./chatRoutes');
 const messageRoutes = require('./messageRoutes');
-const analyticsRoutes = require('./analyticsRoutes');
 
 router.get('/', homeController.getHome);
 router.use('/products', productRoutes);
 router.use('/journals', journalRoutes);
 router.use('/chat', chatRoutes);
 router.use('/messages', messageRoutes);
-router.use('/analytics', analyticsRoutes);
 
 module.exports = router;
