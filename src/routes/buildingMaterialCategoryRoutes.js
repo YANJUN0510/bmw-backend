5493b3050245a1b71ceeae2d279830b0815a2481
YANJUN <ruipucui@gmail.com>
2025-12-29 19:25:54 +1100

    improve material code auto-generation for multi-letter prefixes

--- src/routes/buildingMaterialCategoryRoutes.js
+++ src/routes/buildingMaterialCategoryRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+
 const buildingMaterialCategoryController = require('../controllers/buildingMaterialCategoryController');
 
 // Configure Multer to use memory storage
