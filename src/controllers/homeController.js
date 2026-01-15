508ba1f47408c6c55c1cbede68d7cbd469b4c283
YANJUN <ruipucui@gmail.com>
2026-01-15 11:26:42 +1100

    Initial commit BMW backend API

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,6 @@
 exports.getHome = (req, res) => {
   res.json({
-    message: 'Welcome to the Solidoro Backend API',
+    message: 'Welcome to the BMW Backend API',
     status: 'success',
   });
 };
